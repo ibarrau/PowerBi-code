868bf3964dd021dd5dca714713336ba7f1206767
ibarrau <ibarrau@piconsulting.com.ar>
2019-08-12 13:06:26 -0300

    powershell refresh

---
+++ PowerShell/RefreshPowerBi.ps1
@@ -0,0 +1,14 @@
+﻿$User = "XXXXX@company.com"
+$PW = "secret"
+
+$SecPasswd = ConvertTo-SecureString $PW -AsPlainText -Force
+$myCred = New-Object System.Management.Automation.PSCredential($User,$SecPasswd)
+
+Connect-PowerBIServiceAccount -Credential $myCred
+
+$DSIDRefresh = "XXXX-XXXX-XXXX-XXXX"
+$WSIDAdmin = "XXXX-XXXX-XXXX-XXXX"
+
+$RefreshDSURL = 'groups/' + $WSIDAdmin + '/datasets/' + $DSIDRefresh + '/refreshes'
+
+Invoke-PowerBIRestMethod -Url $RefreshDSURL -Method Post
